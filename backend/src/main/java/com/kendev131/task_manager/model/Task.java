557194f18928dd8205dbe483907f75b89aca5c41
Ken Pederson <102421691+Rift131@users.noreply.github.com>
2026-01-15 18:00:27 -0600

    Merge tutorial branch into main

---
+++ backend/src/main/java/com/kendev131/task_manager/model/Task.java
@@ -0,0 +1,36 @@
+package com.kendev131.task_manager.model;
+
+public class Task {
+
+    private String title;
+    private String description;
+    private boolean completed;
+
+    // Getters & Setters
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+
+}
